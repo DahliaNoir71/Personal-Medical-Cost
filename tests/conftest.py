7fe4d4250a620c7f56f2977aecf1041fd4bbe88b
Serge PFEIFFER <serge.pfeiffer@proton.mail>
2025-03-18 10:36:15 +0100

    Tests unitaires

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from models.base import Base
 
 
 # Configuration de la base de données en mémoire pour les tests
 @pytest.fixture(scope="module")
 def db_session():
-    Base = declarative_base()
     # Créer une base de données SQLite en mémoire
     engine = create_engine("sqlite:///:memory:", echo=True)
     Base.metadata.create_all(engine)
