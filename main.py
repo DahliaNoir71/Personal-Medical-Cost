b9abba78e93ef5deb6bc8d54ebcbc4654e424ff4
Serge PFEIFFER <serge.pfeiffer@proton.mail>
2025-03-20 13:56:19 +0100

    Corrections

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from api.insured import insured as insured_router
+from api.insured import insured_router
 from database.database import create_tables
 from datas.datas import load_datas_from_csv, generate_names_based_on_sex
 from modules.seed import seed_insured
